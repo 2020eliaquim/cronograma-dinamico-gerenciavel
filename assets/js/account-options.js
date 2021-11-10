1ea97b6cff3d4c67d354da2c88ad26d6f7c909d1
eliaquim jordan <eliaquim_jordann@hotmail.com>
2021-11-10 15:53:53 -0400

    criado menu da conta

--- assets/js/account-options.js
+++ assets/js/account-options.js
@@ -1,1 +1,11 @@
-console.log('page options-accounts.js');
+const btnAccountMenu = document.getElementById('icon-menu-account');
+const menuAccount = document.querySelector('.account-options');
+
+btnAccountMenu.addEventListener('click', ()=>{
+    if(!menuAccount.classList.contains('menu-account--show')) {
+        menuAccount.classList.add('menu-account--show');
+        return;
+    } else {
+        menuAccount.classList.remove('menu-account--show');
+    }
+});
